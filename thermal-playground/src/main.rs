600a88387b56d0e070938381fd5b9a24226b0ea5
FN <firenamefn@gmail.com>
2026-06-10 05:05:34 +0700

    Move thermal-playground

---
+++ thermal-playground/src/main.rs
@@ -0,0 +1,101 @@
+use std::{ffi::CStr, slice};
+
+use ash::{
+    Entry,
+    vk::{self, Handle},
+};
+use thermal::{
+    ext::physical_device::ThPhysicalDeviceIteratorExt,
+    thvk::{device::QueueInfo, instance::ThInstance},
+};
+
+fn main() {
+    println!("Hello, world!");
+
+    let instance_extensions;
+
+    unsafe {
+        sdl3_sys::hints::SDL_SetHint(
+            sdl3_sys::hints::SDL_HINT_VIDEO_DRIVER,
+            c"wayland,x11,cocoa,windows".as_ptr(),
+        );
+
+        sdl3_sys::init::SDL_Init(sdl3_sys::init::SDL_InitFlags::VIDEO);
+
+        sdl3_sys::vulkan::SDL_Vulkan_LoadLibrary(std::ptr::null());
+
+        let mut count = 0;
+
+        let f = slice::from_raw_parts(
+            sdl3_sys::vulkan::SDL_Vulkan_GetInstanceExtensions(&mut count),
+            count as usize,
+        );
+
+        instance_extensions = f.iter().map(|ptr| CStr::from_ptr(*ptr)).collect::<Vec<_>>();
+    }
+
+    let entry;
+
+    unsafe {
+        entry = Entry::load().unwrap();
+    }
+
+    let instance = ThInstance::new(
+        &entry,
+        vk::API_VERSION_1_2,
+        &[c"VK_LAYER_KHRONOS_validation"],
+        &instance_extensions,
+    )
+    .unwrap();
+
+    unsafe {
+        for pd in instance
+            .handle
+            .enumerate_physical_devices()
+            .unwrap()
+            .into_iter()
+        {
+            let str = CStr::from_ptr(
+                instance
+                    .handle
+                    .get_physical_device_properties(pd)
+                    .device_name
+                    .as_ptr(),
+            );
+
+            println!("{}", str.to_str().unwrap());
+        }
+    }
+
+    let physical_device;
+    let family;
+
+    unsafe {
+        (physical_device, family) = instance
+            .physical_devices()
+            .unwrap()
+            .filter_discrete()
+            .find_with_queue_family(|device, family, _| {
+                sdl3_sys::vulkan::SDL_Vulkan_GetPresentationSupport(
+                    device.instance.handle.handle().as_raw() as *mut _,
+                    device.handle.as_raw() as *mut _,
+                    family,
+                )
+            })
+            .unwrap();
+    }
+
+    let device = physical_device
+        .create_device(
+            &[QueueInfo {
+                index: family,
+                priorities: &[0.0],
+            }],
+            &[],
+        )
+        .unwrap();
+
+    //std::thread::sleep(Duration::from_secs(60));
+
+    println!("Done");
+}
