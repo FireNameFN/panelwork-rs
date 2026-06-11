05c6e6824dda63eeee58d74524afd4542753fead
FN <firenamefn@gmail.com>
2026-06-11 20:49:27 +0700

    update thermal

--- thermal-playground/src/main.rs
+++ thermal-playground/src/main.rs
@@ -1,6 +1,10 @@
 use std::{ffi::CStr, slice};
 
 use ash::vk::{self, Handle};
+use sdl3_sys::{
+    events::{SDL_Event, SDL_EventType},
+    video::{SDL_WINDOW_RESIZABLE, SDL_WINDOW_VULKAN},
+};
 use thermal::{
     ext::physical_device::ThPhysicalDeviceIteratorExt,
     thvk::{device::QueueInfo, library::ThLibrary},
@@ -41,42 +45,32 @@ fn main() {
         )
         .unwrap();
 
-    unsafe {
-        for pd in instance
-            .handle
-            .enumerate_physical_devices()
-            .unwrap()
-            .into_iter()
-        {
-            let str = CStr::from_ptr(
+    for physical_device in instance.physical_devices().unwrap().into_iter() {
+        let str = unsafe {
+            CStr::from_ptr(
                 instance
                     .handle
-                    .get_physical_device_properties(pd)
+                    .get_physical_device_properties(physical_device.handle)
                     .device_name
                     .as_ptr(),
-            );
+            )
+        };
 
-            println!("{}", str.to_str().unwrap());
-        }
+        println!("{}", str.to_str().unwrap());
     }
 
-    let physical_device;
-    let family;
-
-    unsafe {
-        (physical_device, family) = instance
-            .physical_devices()
-            .unwrap()
-            .filter_discrete()
-            .find_with_queue_family(|device, family, _| {
-                sdl3_sys::vulkan::SDL_Vulkan_GetPresentationSupport(
-                    device.instance.handle.handle().as_raw() as *mut _,
-                    device.handle.as_raw() as *mut _,
-                    family,
-                )
-            })
-            .unwrap();
-    }
+    let (physical_device, family) = instance
+        .physical_devices()
+        .unwrap()
+        .filter_discrete()
+        .find_with_queue_family(|device, family, _| unsafe {
+            sdl3_sys::vulkan::SDL_Vulkan_GetPresentationSupport(
+                device.instance.handle.handle().as_raw() as *mut _,
+                device.handle.as_raw() as *mut _,
+                family,
+            )
+        })
+        .unwrap();
 
     let device = physical_device
         .create_device(
@@ -89,6 +83,37 @@ fn main() {
         .unwrap();
 
     //std::thread::sleep(Duration::from_secs(60));
+
+    unsafe {
+        let mut window = std::ptr::null_mut();
+
+        let mut renderer = std::ptr::null_mut();
+
+        sdl3_sys::render::SDL_CreateWindowAndRenderer(
+            c"Thermal".as_ptr(),
+            1280,
+            720,
+            SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN,
+            &mut window,
+            &mut renderer,
+        );
+
+        'outer: loop {
+            sdl3_sys::events::SDL_WaitEvent(std::ptr::null_mut());
+
+            let mut event = SDL_Event::default();
+
+            while sdl3_sys::events::SDL_PollEvent(&mut event) {
+                if event.event_type() == SDL_EventType::QUIT {
+                    break 'outer;
+                }
+            }
+
+            sdl3_sys::render::SDL_RenderClear(renderer);
+
+            sdl3_sys::render::SDL_RenderPresent(renderer);
+        }
+    }
 
     println!("Done");
 }
